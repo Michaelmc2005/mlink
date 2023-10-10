c3308d3cb9543e888fae4895d94189ba7cb4eb0c
Michael <michaelmc2005@gmail.com>
2023-10-10 14:51:19 +0100

    third

--- src/screens/DesktopWrap.vue
+++ src/screens/DesktopWrap.vue
@@ -91,6 +91,10 @@ export default {
   flex-direction: row;
   justify-content: center;
   width: 100%;
+  position: fixed;
+  transform: scale(0.75);
+  right: 20%;
+  bottom: 17%;
 }
 
 .desktop .div-2 {
